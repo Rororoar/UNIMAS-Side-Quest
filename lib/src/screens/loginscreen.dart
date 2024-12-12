7fb9ff436c792e5c701e94b188de279e3b559e5b
Milizee <gridiffin6603@gmail.com>
2024-12-12 20:12:39 +0800

    git commit

--- lib/src/screens/loginscreen.dart
+++ lib/src/screens/loginscreen.dart
@@ -5,6 +5,8 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,6 +16,8 @@ class MyApp extends StatelessWidget {
 }
 
 class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
   @override
   _LoginScreenState createState() => _LoginScreenState();
 }
@@ -29,11 +33,11 @@ class _LoginScreenState extends State<LoginScreen> {
     if (username == "admin" && password == "1234") {
       // Simulate a successful login
       ScaffoldMessenger.of(context)
-          .showSnackBar(SnackBar(content: Text('Login Successful')));
+          .showSnackBar(const SnackBar(content: Text('Login Successful')));
     } else {
       // Simulate a failed login
       ScaffoldMessenger.of(context)
-          .showSnackBar(SnackBar(content: Text('Invalid credentials')));
+          .showSnackBar(const SnackBar(content: Text('Invalid credentials')));
     }
   }
 
@@ -41,25 +45,25 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Login'),
+        title: const Text('Login'),
       ),
       body: Padding(
-        padding: EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
             TextField(
               controller: _usernameController,
-              decoration: InputDecoration(labelText: 'Username'),
+              decoration: const InputDecoration(labelText: 'Username'),
             ),
             TextField(
               controller: _passwordController,
-              decoration: InputDecoration(labelText: 'Password'),
+              decoration: const InputDecoration(labelText: 'Password'),
               obscureText: true,
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             ElevatedButton(
               onPressed: _login,
-              child: Text('Login'),
+              child: const Text('Login'),
             ),
           ],
         ),
